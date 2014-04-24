18d6b2b7751994fda21bc7acbd10bb9bcb8547b4
wsds <wsds888@163.com>
2014-04-24 12:06:21 +0800

    add some files

--- hello-gl2/CppSource/gl_code.cpp
+++ hello-gl2/CppSource/gl_code.cpp
@@ -51,7 +51,7 @@ static const char gVertexShader[] =
 static const char gFragmentShader[] = 
     "precision mediump float;\n"
     "void main() {\n"
-    "  gl_FragColor = vec4(0.0, 1.0, 1.0, 0.0);\n"
+    "  gl_FragColor = vec4(1.0, 1.0, 0.0, 0.0);\n"
     "}\n";
 
 GLuint loadShader(GLenum shaderType, const char* pSource) {
