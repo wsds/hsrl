d0881f5e9437bf48b7377ae9f0f8e2ace2d9836c
wsds <wsds888@163.com>
2014-04-25 12:39:24 +0800

    The puzzle has been resolved.

--- hsrl-frame/CppSource/MainEngine.cpp
+++ hsrl-frame/CppSource/MainEngine.cpp
@@ -3,23 +3,15 @@
 
 namespace hsrl {
 
-
-	//MainEngine::MainEngine()
-	//{
-	//}
+	MainEngine::MainEngine()
+	{
+	}
 
 
 	MainEngine::~MainEngine()
 	{
 	}
-	int MainEngine::test = 1;
-	int MainEngine::test1 = 100;
 
-	//MainEngine * MainEngine::getInstance() {
-	//	if (MainEngine::mMainEngine == NULL) {
-	//		MainEngine::mMainEngine = new MainEngine();
-	//	}
-	//	return MainEngine::mMainEngine;
-	//}
+	MainEngine* MainEngine::instance = NULL;
 
 }
--- hsrl-frame/CppSource/hsrl/tools.cpp
+++ hsrl-frame/CppSource/hsrl/tools.cpp
@@ -10,48 +10,11 @@
 namespace hsrl {
 
 
-	class A{
-	private:
-		int w, h;
-		int m;
-	public:
-		static int area;//静态数据成员的定义形式
-		int area1;
-		void setVal(int x)
-		{
-			area = x;
-		}
-		int getArea()
-		{
-			return area;
-		}
-		A(int x, int y)
-		{
-			w = x, h = y;
-		}
-		A(){}
-	};
-	int A::area = 0;
-	void test2(){
-		A const a(3, 4);
-		A c(2, 6);
-		A f;
-		c.area1 = 4;
-		c.setVal(4);
-		c.area = 8;//初始化后可以改变其值，不初始化程序编译都通不过
-		f.area = 16;
-	}
-	//int MainEngine::test1 = 100;
+	
 	void test() {
 		SpaceHolder* spaceHolder = new SpaceHolder();
 		spaceHolder->test();
-		/*	MainEngine* m = new MainEngine();
-			m->test1 = 1;*/
-		//MainEngine m();
-		//m.test1 = 1; 
-		//hsrl::MainEngine* mMainEngine = hsrl::MainEngine::getInstance();
-		MainEngine::test1 = 120;
-
+		hsrl::MainEngine* mMainEngine = hsrl::MainEngine::getInstance();
 
 	}
 
