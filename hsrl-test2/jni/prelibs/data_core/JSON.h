6c2d08daa3431a4c0c444c4d9b2eec1e9bba9b7d
wsds <wsds888@163.com>
2014-06-12 16:29:14 +0800

    Data core base element

---
+++ hsrl-test2/jni/prelibs/data_core/JSON.h
@@ -0,0 +1,24 @@
+#ifndef JSON_H
+#define JSON_H
+
+
+#include "JSObject.h"
+
+#ifndef NULL
+#define NULL 0;
+#endif /* NULL */
+
+class JSON : JSObject
+{
+
+
+	void test(){
+
+		int i = 1 + 1;
+	}
+
+};
+
+
+#endif /* JSON_H */
+
---
+++ hsrl-test2/jni/prelibs/data_core/JSObject.h
@@ -0,0 +1,16 @@
+#ifndef JSOBJECT_H
+#define JSOBJECT_H
+
+
+#ifndef NULL
+#define NULL 0;
+#endif /* NULL */
+
+class JSObject
+{
+
+};
+
+
+#endif /* JSOBJECT_H */
+
