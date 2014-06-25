a94f05d29954aa3e4ee8545128ea89d071390668
wsds <wsds888@163.com>
2014-06-25 11:49:44 +0800

    A tree shaped structure that hold by JSON and its indicator is resolved by spite string with COMMA.

--- ConsoleApplication1/data_core/JSON.cpp
+++ ConsoleApplication1/data_core/JSON.cpp
@@ -178,17 +178,18 @@ bool JSON::parse(char* string)
 void testJSONParse(){
 
 	char * json_str = "[a:1,'''abc''',123,[1,2,[a:1,b:[123,[1,12],456],123],'a':2,'b:3':\"123456\"],'''a''', '''''']";
+	char * json_str1 = "[123,[1,12,13,[123,[1,12],456],14,15,16,17],456,789,101112]";
 
 	JSONIndicator* root_json_indicator = new JSONIndicator();
 	root_json_indicator->head = 0;
-	root_json_indicator->tail = strlen(json_str);
+	root_json_indicator->tail = strlen(json_str1);
 
 	JSON* json = new JSON();
 	json->initialize();
 
 	root_json_indicator->json = (JSObject*)json;
 
-	JSON* josn = parseJSON(json_str, root_json_indicator);
+	JSON* josn = parseJSON(json_str1, root_json_indicator);
 
 }
 int parsingStatus = 1;//[1:normal,2:quote_start,3:string,4:second_quote_start,5:quote_stop,6:second_quote_stop],[1-2,2-3,3-1,2-4,4-5,5-3,4-1]
@@ -210,26 +211,29 @@ JSON* parseJSON(char* string, JSONIndicator* root_json_indicator){
 	char localChar;
 	last_COMMA_index = 0;
 
-	JSONIndicator* json_indicator = root_json_indicator;
+	JSONIndicator* json_indicator = NULL;
+	JSON* result_json = NULL;
 
 	int mem_size = json_indicators_stack_size*sizeof(JSONIndicator*);
 	JSONIndicator** json_indicators = (JSONIndicator**)JSMalloc(mem_size);
 
-	for (int i = json_indicator->head; i < root_json_indicator->tail; i++){
+	for (int i = root_json_indicator->head; i < root_json_indicator->tail; i++){
 		localChar = string[i];
 		if (parsingStatus == 1){
 			//*************************************parsingStatus == 1*****************************************************************************************************
 			if (localChar == COMMA){
+				if (i>last_COMMA_index){
+					JSON* json = new JSON();
+					json->json_indicator = new JSONIndicator();//get from pool//to do
+					json->json_indicator->head = last_COMMA_index;
+					json->json_indicator->tail = i;
+					json->json_indicator->json = (JSObject*)json;
 
-				JSON* json = new JSON();
-				json->json_indicator = new JSONIndicator();//get from pool//to do
-				json->json_indicator->head = last_COMMA_index;
-				json->json_indicator->tail = i;
-				json->json_indicator->json = (JSObject*)json;
+					((JSON*)(json_indicator->json))->push((JSObject*)json);
 
-				((JSON*)(json_indicator->json))->push((JSObject*)json);
-				//resolve elememt
-
+					last_COMMA_index = i + 1;
+					//resolve elememt
+				}
 			}
 			else if (localChar == SINGLEQUOTE){
 				QuoteStatus = 11;
@@ -250,23 +254,58 @@ JSON* parseJSON(char* string, JSONIndicator* root_json_indicator){
 					json->json_indicator->head = i;
 					json->json_indicator->json = (JSObject*)json;
 
-					((JSON*)(json_indicator->json))->push((JSObject*)json);
+					if (i == 0 && json_indicator == NULL){
+						result_json = json;
+					}
+					else{
+						((JSON*)(json_indicator->json))->push((JSObject*)json);
+					}
+
 
 					json_indicators[json_indicators_stack_top] = json->json_indicator;
 					json_indicators_stack_top++;
 
 					json_indicator = json->json_indicator;
+
+					last_COMMA_index++;
 				}
 				else{
 					//report error
 				}
 			}
 			else if (localChar == RIGHTBRACKET){
-				if (json_indicators_stack_top > 0){
+				JSON* json = new JSON();
+				json->json_indicator = new JSONIndicator();//get from pool//to do
+				json->json_indicator->head = last_COMMA_index;
+				json->json_indicator->tail = i;
+				json->json_indicator->json = (JSObject*)json;
+
+				((JSON*)(json_indicator->json))->push((JSObject*)json);
+				if (i + 1 == root_json_indicator->tail || string[i + 1] == COMMA){
+					last_COMMA_index = i + 2;
+				}
+
+
+				if (json_indicators_stack_top > 1){
+					//resolve the last element spited by COMMA
 					json_indicator = json_indicators[json_indicators_stack_top - 1];
 					json_indicator->tail = i;
+
+					json_indicator = json_indicators[json_indicators_stack_top - 2];
 					//json_indicator->json = parseJSON(string, json_indicator);
 					json_indicators_stack_top--;
+				}
+				else if (json_indicators_stack_top = 1){
+					if (i + 1 == root_json_indicator->tail){
+						//resolve the last element spited by COMMA
+						int p = 10;
+						p++;
+						break;
+						//return result_json;
+					}
+					else{
+						//report error
+					}
 				}
 				else{
 					//report error
